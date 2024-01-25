b63a97aa04c25c12f110834e550d1547ab41a537
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-01-25 21:37:51 +0200

    solved the bug with packages

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -9,9 +9,9 @@ contract RealEstate is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721 ("Real Estate", "REAL") public {}
+    constructor() ERC721("Real Estate", "REAL") {}
 
-    function mint () public returns (uint256) {
+    function mint(string memory tokenURI) public returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
@@ -21,7 +21,7 @@ contract RealEstate is ERC721URIStorage {
         return newItemId;
     }
 
-    function totalSupply () public view returns (uint256) {
+    function totalSupply() public view returns (uint256) {
         return _tokenIds.current();
     }
 }
