94b1b031003dea69fea8a842db9be03de9b9ef7b
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-01-29 07:47:05 +0200

    finished completely the smart contract, deploying it to the local blockchain

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -68,12 +68,6 @@ contract Escrow {
         approval[_nftID][msg.sender] = true;
     }
 
-    receive() external payable {}
-
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
-    }
-
     function finalizeSale(uint256 _nftID) public {
         require(inspectionPassed[_nftID]);
         require(approval[_nftID][buyer[_nftID]]);
@@ -81,9 +75,25 @@ contract Escrow {
         require(approval[_nftID][lender]);
         require(address(this).balance >= purchasePrice[_nftID]);
 
+        isListed[_nftID] = false;
+
         (bool success, ) = payable(seller).call{ value: address(this).balance }("");
         require(success);
 
         IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
+    }
+
+    function cancelSale(uint256 _nftID) public {
+        if (inspectionPassed[_nftID] == false) {
+            payable(buyer[_nftID]).transfer(address(this).balance);
+        } else {
+            payable(seller).transfer(address(this).balance);
+        }
+    }
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
