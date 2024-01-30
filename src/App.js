16cab59195516eb8e9b395351ddc1ce8d8a6c9b5
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-01-30 10:04:24 +0200

    finished rendering and updating the accounts

--- src/App.js
+++ src/App.js
@@ -19,9 +19,12 @@ function App() {
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0]);
-    // console.log(account);
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
   }
 
   useEffect(() => {
@@ -30,6 +33,8 @@ function App() {
 
   return (
     <div>
+
+      <Navigation account={account} setAccount={setAccount} />
 
       <div className='cards__section'>
 
